18e7c62ec78ee16188682ae7038ca8782fa3fa6a
NominalHydroxides <aaronvork22@gmail.com>
2026-09-03 13:41:15 +0300

    DbContext

---
+++ ShopTAR25/ShopTARpe25.Data/ShopTARpe25Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShopTARpe25.Data
+{
+    public class ShopTARpe25Context : DbContext
+    {
+        public ShopTARpe25Context(
+            DbContextOptions<ShopTARpe25Context> options)
+            : base(options)
+        {
+        }
+        //teha DbSet, et saaks andmebaasi kasutada
+        
+    }
+}
