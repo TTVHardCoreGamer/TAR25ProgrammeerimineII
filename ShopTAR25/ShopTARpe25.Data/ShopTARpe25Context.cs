329ef22a20e4e48a145f61964db5e11abdfbc7db
TTVHardCoreGamer <aaronvork22@gmail.com>
2026-09-03 13:58:10 +0300

    SpaceshipDomain

--- ShopTAR25/ShopTARpe25.Data/ShopTARpe25Context.cs
+++ ShopTAR25/ShopTARpe25.Data/ShopTARpe25Context.cs
@@ -7,12 +7,12 @@ namespace ShopTARpe25.Data
 {
     public class ShopTARpe25Context : DbContext
     {
-        public ShopTARpe25Context(
-            DbContextOptions<ShopTARpe25Context> options)
+        public ShopTARpe25Context(DbContextOptions<ShopTARpe25Context> options)
             : base(options)
         {
         }
         //teha DbSet, et saaks andmebaasi kasutada
-        
+        //teha Core projekti alla Domain nimega kaust ja sinna class
+        //nimega Spaceship
     }
 }
