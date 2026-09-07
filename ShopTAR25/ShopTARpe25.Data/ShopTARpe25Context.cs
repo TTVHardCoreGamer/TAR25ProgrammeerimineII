e50d79c7dbadd5643b6bec7c895e991f7956a20c
Opilane Techno+TLN <aaronvork22@gmail.com>
2026-09-07 14:10:41 +0300

    SpaceshipDtoAndContext

--- ShopTAR25/ShopTARpe25.Data/ShopTARpe25Context.cs
+++ ShopTAR25/ShopTARpe25.Data/ShopTARpe25Context.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using ShopTARpe25.Core.Domain;
 
 namespace ShopTARpe25.Data
 {
@@ -12,7 +10,6 @@ namespace ShopTARpe25.Data
         {
         }
         //teha DbSet, et saaks andmebaasi kasutada
-        //teha Core projekti alla Domain nimega kaust ja sinna class
-        //nimega Spaceship
-    }
+        public DbSet<Spaceship> Spaceships { get; set; }
+     }
 }
